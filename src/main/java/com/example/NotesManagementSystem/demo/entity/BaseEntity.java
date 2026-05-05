d0c210801f297773fae7c9405bc3803163cea6c1
whoisyashu <maheshwariy077@gmail.com>
2026-05-05 16:25:42 +0530

    entity files:  Base Entity

---
+++ src/main/java/com/example/NotesManagementSystem/demo/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.example.NotesManagementSystem.demo.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
