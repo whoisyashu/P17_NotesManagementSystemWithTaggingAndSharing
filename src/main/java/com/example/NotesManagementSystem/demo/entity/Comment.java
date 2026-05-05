122c13cc4cd78c948e5fcbbc971c1f4e53ff51f9
whoisyashu <maheshwariy077@gmail.com>
2026-05-05 16:26:19 +0530

    entity files: Comment Entity

---
+++ src/main/java/com/example/NotesManagementSystem/demo/entity/Comment.java
@@ -0,0 +1,44 @@
+package com.example.NotesManagementSystem.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "comments", indexes = {
+        @Index(name = "idx_comments_note", columnList = "note_id"),
+        @Index(name = "idx_comments_created_at", columnList = "createdAt")
+})
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "note_id", nullable = false)
+    private Note note;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String content;
+}
